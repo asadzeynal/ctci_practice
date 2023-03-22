1f665930d54bcf167a96bf463aba6acc24c80bd9
asadzeynal <asadzeynal9@gmail.com>
2023-03-23 03:34:23 +0400

    Added a faster method for prac_1_7

--- prac_1_7.go
+++ prac_1_7.go
@@ -2,7 +2,6 @@ package main
 
 func rotateMatrix(matrix [][]int) [][]int {
 	level := len(matrix) - 1
-
 	numOfCycles := level
 	for deepness := 0; deepness < level-1; deepness++ {
 		i, j := deepness, deepness
@@ -32,6 +31,25 @@ func rotateMatrix(matrix [][]int) [][]int {
 			}
 		}
 		numOfCycles -= 2
+	}
+
+	return matrix
+}
+
+func rotateMatrixFaster(matrix [][]int) [][]int {
+	n := len(matrix) - 1
+	for depth := 0; depth < len(matrix)-2; depth++ {
+		for offset := 0; offset < n-(depth*2); offset++ {
+			el1 := matrix[depth][depth+offset]
+			el2 := matrix[depth+offset][n-depth]
+			el3 := matrix[n-depth][n-depth-offset]
+			el4 := matrix[n-depth-offset][depth]
+
+			matrix[depth][depth+offset] = el4
+			matrix[depth+offset][n-depth] = el1
+			matrix[n-depth][n-depth-offset] = el2
+			matrix[n-depth-offset][depth] = el3
+		}
 	}
 
 	return matrix
--- prac_1_7_test.go
+++ prac_1_7_test.go
@@ -10,7 +10,10 @@ func TestRotateMatrix(t *testing.T) {
 		s    [][]int
 		want [][]int
 	}{
-		{[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, [][]int{{7, 4, 1}, {8, 5, 2}, {9, 6, 3}}},
+		{
+			[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+			[][]int{{7, 4, 1}, {8, 5, 2}, {9, 6, 3}},
+		},
 		{
 			[][]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}},
 			[][]int{{13, 9, 5, 1}, {14, 10, 6, 2}, {15, 11, 7, 3}, {16, 12, 8, 4}},
@@ -25,6 +28,36 @@ func TestRotateMatrix(t *testing.T) {
 		testname := fmt.Sprintf("%v", tt.s)
 		t.Run(testname, func(t *testing.T) {
 			ans := rotateMatrix(tt.s)
+			if !EqualMatrix(ans, tt.want) {
+				t.Errorf("got %v, want %v", ans, tt.want)
+			}
+		})
+	}
+}
+
+func TestRotateMatrixFaster(t *testing.T) {
+	tests := []struct {
+		s    [][]int
+		want [][]int
+	}{
+		{
+			[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+			[][]int{{7, 4, 1}, {8, 5, 2}, {9, 6, 3}},
+		},
+		{
+			[][]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}},
+			[][]int{{13, 9, 5, 1}, {14, 10, 6, 2}, {15, 11, 7, 3}, {16, 12, 8, 4}},
+		},
+		{
+			[][]int{{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}},
+			[][]int{{21, 16, 11, 6, 1}, {22, 17, 12, 7, 2}, {23, 18, 13, 8, 3}, {24, 19, 14, 9, 4}, {25, 20, 15, 10, 5}},
+		},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%v", tt.s)
+		t.Run(testname, func(t *testing.T) {
+			ans := rotateMatrixFaster(tt.s)
 			if !EqualMatrix(ans, tt.want) {
 				t.Errorf("got %v, want %v", ans, tt.want)
 			}
