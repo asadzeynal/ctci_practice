469ed66d2a1503cd58da1e94de92a4c7e72d53b8
asadzeynal <asadzeynal9@gmail.com>
2023-03-23 05:24:40 +0400

    util LinkedList slice converters implemented

--- util/util.go
+++ util/util.go
@@ -1,5 +1,10 @@
 package util
 
+type Node struct {
+	Data int
+	Next *Node
+}
+
 func EqualSlice(a, b []int) bool {
 	if len(a) != len(b) {
 		return false
@@ -23,4 +28,30 @@ func EqualMatrix(a, b [][]int) bool {
 		}
 	}
 	return true
+}
+
+func LinkedListFromSlice(s []int) *Node {
+	if len(s) == 0 {
+		return nil
+	}
+	head := &Node{}
+	n := head
+	for i := range s {
+		n.Data = s[i]
+		if i == len(s)-1 {
+			n.Next = nil
+			break
+		}
+		n.Next = &Node{}
+		n = n.Next
+	}
+	return head
+}
+
+func SliceFromLinkedList(n *Node) []int {
+	result := make([]int, 0)
+	for n != nil {
+		result = append(result, n.Data)
+	}
+	return result
 }
