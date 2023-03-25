157ef84165fd4febb2335b7f11f9130a60641b2d
asadzeynal <asadzeynal9@gmail.com>
2023-03-25 16:01:52 +0400

    improved queue

--- chapter4/prac_4_1.go
+++ chapter4/prac_4_1.go
@@ -14,17 +14,18 @@ func routeBetweenNodes(node1 *GraphNode, node2 *GraphNode) bool {
 	queue := make([]*GraphNode, 1)
 	queue[0] = node1
 	node1.Marked = true
-	for i := 0; i < len(queue); i++ {
-		if queue[i] == node2 {
+	for len(queue) != 0 {
+		if queue[0] == node2 {
 			return true
 		}
-		for _, a := range queue[i].Adjacent {
+		for _, a := range queue[0].Adjacent {
 			if a.Marked {
 				continue
 			}
 			a.Marked = true
 			queue = append(queue, a)
 		}
+		queue = queue[1:]
 	}
 
 	return false
